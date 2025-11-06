d022a4200267f6d6e7c8ddc53eb22f7c96103d9d
KabboudiW <Wael.Kabboudi@Student.HTW-Berlin.de>
2025-11-06 16:35:28 +0100

    VITE-M35

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string
-  // Add any other VITE_ prefixed env variables here
+  // Add other VITE_ prefixed env variables here
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
