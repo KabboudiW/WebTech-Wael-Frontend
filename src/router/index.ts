2a0c72d88dc14003c565b2f6bb68a97542ed0e67
KabboudiW <Wael.Kabboudi@Student.HTW-Berlin.de>
2026-01-17 20:36:04 +0100

    leagues

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 // router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CategoryView from '../views/CategoryView.vue'
+import LeagueView from '../views/LeagueView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
 
   // ✅ ändern: league statt category
-  { path: '/league/:code', component: CategoryView },
+  { path: '/league/:code', component: LeagueView },
 
   { path: '/about', component: AboutView }
 ]
